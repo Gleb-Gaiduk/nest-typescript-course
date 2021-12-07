7cc5346b8b54dc28e0efff6f68dda5cb4b1df442
hajdukh <hleb.hajduk@contractors.roche.com>
2021-12-07 16:49:02 +0100

    Add JWT token basic config

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '../config';
+import { ConfigService } from './../config/config.service';
 import { UsersAdminController } from './controllers/user-admin.controller';
 import { AuthController } from './controllers/user-auth.controller';
 import { UsersController } from './controllers/users.controller';
@@ -6,6 +9,16 @@ import { AuthService } from './services/auth.service';
 import { UsersService } from './services/users.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.JWT_SECRET,
+        signOptions: { expiresIn: '4d' },
+      }),
+    }),
+  ],
   controllers: [UsersController, AuthController, UsersAdminController],
   providers: [UsersService, AuthService],
   exports: [UsersService],
