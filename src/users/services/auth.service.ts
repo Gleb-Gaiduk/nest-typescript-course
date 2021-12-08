7f9c9a26ac95714b15102d30760cb53e3010cab7
hajdukh <hleb.hajduk@contractors.roche.com>
2021-12-08 09:34:43 +0100

    Add methods for password encoding

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -17,8 +17,19 @@ export class AuthService {
     email: string,
     password: string,
   ): Promise<User | null> {
-    const [user] = await this.usersService.findBy({ email, password });
+    const [user] = await this.usersService.findBy({ email });
+
+    if (!this.validatePassword(password, user.password)) return null;
+
     return user;
+  }
+
+  encodePassword(password: string) {
+    return password;
+  }
+
+  validatePassword(password: string, hash: string) {
+    return password === hash;
   }
 
   async encodeUserToken(user: User): Promise<string> {
