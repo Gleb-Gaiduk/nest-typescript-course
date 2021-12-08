9d439759a16aaaa75a7b564093f2429b0267bd00
hajdukh <hleb.hajduk@contractors.roche.com>
2021-12-08 10:21:05 +0100

    Edit dto mocked example data

--- src/users/dto/auth.dto.ts
+++ src/users/dto/auth.dto.ts
@@ -2,9 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../entities/user.entity';
 
 export class AuthLoginDto {
-  @ApiProperty({ example: 'Piotr' })
-  email: string;
   @ApiProperty({ example: 'piotr@myflow.pl' })
+  email: string;
+  @ApiProperty({ example: '123' })
   password: string;
 }
 
@@ -14,11 +14,11 @@ export class AuthLoginResponse {
 }
 
 export class AuthRegisterDto {
-  @ApiProperty({ example: 'Justyna' })
+  @ApiProperty({ example: 'Piotr' })
   name: string;
   @ApiProperty({ example: 'Justyna@gmail.com' })
   email: string;
-  @ApiProperty({ example: 'dfsdf2fds' })
+  @ApiProperty({ example: '123' })
   password: string;
 }
 
