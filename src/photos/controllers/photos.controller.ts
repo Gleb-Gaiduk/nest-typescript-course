4c0e073071da5d7e55d4075191ed0bc536cd3ce8
hajdukh <hleb.hajduk@contractors.roche.com>
2021-12-08 14:39:31 +0100

    Set up swagger for file upload endpoint

--- src/photos/controllers/photos.controller.ts
+++ src/photos/controllers/photos.controller.ts
@@ -6,13 +6,16 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { FileUploadDto } from '../dto/photos.dto';
 
 @Controller('photos')
 @ApiTags('Photos')
 export class PhotosController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: FileUploadDto })
   async uploadFile(
     @UploadedFile() file: Express.Multer.File,
     @Body() body: any,
